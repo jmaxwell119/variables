624217a5de06d64e3914562ea11d38ca4d36e4f7
Jason Maxwell <jmaxwell119@students.mchenry.edu>
2020-08-26 17:56:59 -0500

    Initial commit

---
+++ src/main.kt
@@ -0,0 +1,23 @@
+fun main(){
+    val myString= "Woof"
+    val myByte = 122
+    val myShort = 14442
+    val myInt = 5222222
+    val myLong = 245255222225L
+    val myFloat = 12.23F
+    val myDouble = 252525.355
+    val myBoolean = true
+    val myChar = '#'
+
+    println("This is a string: $myString")
+    println("This is a byte: $myByte")
+    println("This is a short: $myShort")
+    println("This is an int: $myInt")
+    println("This is a long: $myLong")
+    println("This is a float: $myFloat")
+    println("This is a double: $myDouble")
+    println("This is a boolean: $myBoolean")
+    println("This is a char: $myChar")
+
+
+}
